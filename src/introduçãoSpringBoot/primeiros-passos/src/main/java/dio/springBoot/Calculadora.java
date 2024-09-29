859e4760f00479c76e2d5dd731a6d139a0cc1dd8
douglas <douglasmory10@gmail.com>
2024-09-29 20:21:43 -0300

    Spring boot, primeiros passos

---
+++ src/introduçãoSpringBoot/primeiros-passos/src/main/java/dio/springBoot/Calculadora.java
@@ -0,0 +1,11 @@
+package dio.springBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+
+    public int soma(int n1, int n2){
+        return n1 + n2;
+    }
+}
