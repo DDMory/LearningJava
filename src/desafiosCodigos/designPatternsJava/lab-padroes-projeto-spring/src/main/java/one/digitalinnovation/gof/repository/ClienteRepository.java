829f06169abdf99de9759334689f222fd09e7e4d
douglas <douglasmory10@gmail.com>
2024-10-04 10:53:31 -0300

    Spring patterns project

---
+++ src/desafiosCodigos/designPatternsJava/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.gof.repository;
+
+import one.digitalinnovation.gof.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {}
