933df48417c28a7fb799193a1cab1f25a4708a0b
douglas <douglasmory10@gmail.com>
2024-10-05 19:00:05 -0300

    Exception treat

---
+++ src/desafiosCodigos/lab-claro-java/src/main/java/me/dio/exception/GlobalExeptionHandler.java
@@ -0,0 +1,35 @@
+package me.dio.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExeptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExeptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> HandleBusinessException(IllegalArgumentException businessExeption){
+        return new ResponseEntity<>(businessExeption.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> HandleNotFoundException(NoSuchElementException noFoundException){
+        return new ResponseEntity<>("RESOUCE ID NOT FOUND", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> HandleUnexpectedException(Throwable unexpectedException){
+        var message = "UNEXPETED SERVER ERROR, SEE THE LOG";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+
+}
